012d8230547724bc2fd5ab52bcf62d251969f8f2
角谷悠平 <oyata1964kado@gmail.com>
2021-06-13 20:33:07 +0900

    first commit

---
+++ FlashAnzan/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlashAnzan
+//
+//  Created by 角谷　悠平 on 2021/06/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
