1ac85db0c74015a14f5d30fc3ed66dbab988b3ca
角谷悠平 <oyata1964kado@gmail.com>
2021-06-26 20:48:24 +0900

    桁数入力機能(ドラムロール)の作成

--- FlashAnzan/View/HomeView.swift
+++ FlashAnzan/View/HomeView.swift
@@ -10,41 +10,46 @@ import UIKit
 @IBDesignable
 class HomeView: UIView {
     
-    @IBOutlet weak var numberOfQuestionsText: UITextField!
-    @IBOutlet weak var digitText: UITextField!
-    @IBOutlet weak var displayIntervalText: UITextField!
+    @IBOutlet weak var numberOfQuestionsTextField: UITextField!
+    @IBOutlet weak var digitTextField: UITextField!
+    @IBOutlet weak var displayIntervalTextField: UITextField!
     
-    var digitPicker = UIPickerView()
-    let digits:[Int] = ([Int])(1...5)
+    var digits:[Int] = ([Int])(1...5)
+    let digitPicker = UIPickerView()
+    var saveValueDigit:Int = 0
     
     required override init(frame: CGRect) {
         super.init(frame: frame)
         loadXib()
+        createDigitPicker(digits: digits)
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         loadXib()
+        createDigitPicker(digits: digits)
     }
     
-
-
     
-    
-    
-    func createDigitPicker() {
-        digitPicker.delegate = self
-        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: frame.width, height: 40))
-        let doneButtonSystemItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: nil)
-        let doneButtonStyleItem = UIBarButtonItem(title: "完了", style: .plain, target: self, action: #selector(done))
-        toolbar.setItems([doneButtonSystemItem,doneButtonStyleItem], animated: true)
+    func createDigitPicker(digits:[Int]) {
+        //self.digits = digits
         
-        digitText.inputView = digitPicker
-        digitText.inputAccessoryView = toolbar
+        digitPicker.delegate = self
+        digitPicker.dataSource = self
+        
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: frame.width, height: 40))
+        let doneButtonSystemItem = UIBarButtonItem(title: "完了", style: .done, target: self, action: #selector(didTapDone))
+        toolbar.setItems([doneButtonSystemItem], animated: true)
+        
+        digitTextField.inputView = digitPicker
+        digitTextField.inputAccessoryView = toolbar
     }
     
-    @objc func done() {
-        endEditing(true)
+    @objc func didTapDone() {
+        digitTextField.resignFirstResponder()
+        let index = digitPicker.selectedRow(inComponent: 0)
+        digitTextField.text = String(digits[index])
+        saveValueDigit = digits[index]
     }
     
     private func loadXib() {
@@ -69,5 +74,7 @@ extension HomeView: UIPickerViewDelegate, UIPickerViewDataSource {
         return digits.count
     }
     
-    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(digits[row])
+    }
 }
